3bf527ffe8c1685fe76d2cf1ee9daf2db3c182dc
mettlenunoo <40646025+mettlenunoo@users.noreply.github.com>
2022-05-03 22:13:48 +0000

    update
    update

--- counter-app/index.js
+++ counter-app/index.js
@@ -1,15 +1,15 @@
-// document.getElementById("count-el").innerText = 5
-
-// change the count-el in the HTML to reflect the new count
-
-// camelCase
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
-
-console.log(countEl)
-
 let count = 0
 
 function increment() {
-    count = count + 1
-    countEl.innerText = count
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
 }
